3d749b0e9bf9b1e4ac3393295c5999cfe0953f6b
Andre Riyanto <andreriyantoo19@gmail.com>
2023-10-19 12:48:15 +0700

    slicing to card create post

---
+++ src/component/card/card-create-post/card-create.jsx
@@ -0,0 +1,14 @@
+export const CardPost = () => {
+  return (
+    <div>
+      <div className="flex gap-2">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJOv79B9ofdIgartgKH5x2ua6ONvrQ-BV32CaLFVHOlYLN31Nm0BUDWv2PabLinvSbcHk&usqp=CAU"
+          alt=""
+        />
+        <input type="text" />
+      </div>
+      <div></div>
+    </div>  
+  );
+};
